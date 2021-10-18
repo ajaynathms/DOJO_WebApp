22f4c65b488e07d64cca20084149c1f26981293e
ajaynathms@outlook.com <ajaynathms@outlook.com>
2021-10-18 01:51:29 +0000

    added test case

--- WebApp.Tests/UnitTest1.cs
+++ WebApp.Tests/UnitTest1.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using WebApp.Models;
 
 namespace WebApp.Tests
 {
@@ -7,8 +11,214 @@ namespace WebApp.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_FirstName_Required()
         {
+            User cusotmer = new User
+            {
+                LastName = "David",
+                Email = "test@test.com",
+                City = "Albany",
+                PhoneNumber = "(999)123-4567",
+                DOB = new DateTime(2000, 12, 5),
+                State = "New York",
+                Country = "United States",
+                StreetAddress1 = "Test",
+                ZipCode = "12345"
+            };
+            var validationResultList = new List<ValidationResult>();
+            Validator.TryValidateObject(cusotmer, new ValidationContext(cusotmer), validationResultList);
+            var message = validationResultList[0].ErrorMessage;
+            Assert.AreEqual("First Name is required", message);
         }
+        [TestMethod]
+        public void Test_LastName_Required()
+        {
+            User cusotmer = new User
+            {
+                FirstName = "John",
+                Email = "test@test.com",
+                City = "Albany",
+                PhoneNumber = "(999)123-4567",
+                DOB = new DateTime(2000, 12, 5),
+                State = "New York",
+                Country = "United States",
+                StreetAddress1 = "Test",
+                ZipCode = "12345"
+            };
+            var validationResultList = new List<ValidationResult>();
+            Validator.TryValidateObject(cusotmer, new ValidationContext(cusotmer), validationResultList);
+            var message = validationResultList[0].ErrorMessage;
+            Assert.AreEqual("Last Name is required", message);
+        }
+
+        [TestMethod]
+        public void Test_Email_Invalid()
+        {
+            User cusotmer = new User
+            {
+                FirstName = "John",
+                LastName = "David",
+                Email = "testcom",
+                City = "Albany",
+                PhoneNumber = "(999)123-4567",
+                DOB = new DateTime(2000, 12, 5),
+                State = "New York",
+                Country = "United States",
+                StreetAddress1 = "Test",
+                ZipCode = "12345"
+            };
+            var validationResultList = new List<ValidationResult>();
+            Validator.TryValidateObject(cusotmer, new ValidationContext(cusotmer), validationResultList);
+            var message = validationResultList[0].ErrorMessage;
+            Assert.AreEqual("Invalid Email", message);
+        }
+        [TestMethod]
+        public void Test_Phone_Invalid()
+        {
+            User cusotmer = new User
+            {
+                FirstName = "John",
+                LastName = "David",
+                Email = "test@test.com",
+                City = "Albany",
+                PhoneNumber = "146",
+                DOB = new DateTime(2000, 12, 5),
+                State = "New York",
+                Country = "United States",
+                StreetAddress1 = "Test",
+                ZipCode = "12345"
+            };
+            var validationResultList = new List<ValidationResult>();
+            Validator.TryValidateObject(cusotmer, new ValidationContext(cusotmer), validationResultList);
+            var message = validationResultList[0].ErrorMessage;
+            Assert.AreEqual("Invalid PhoneNumber", message);
+        }
+        [TestMethod]
+        public void Test_StreetAddress1_Required()
+        {
+            User cusotmer = new User
+            {
+                FirstName = "John",
+                LastName = "David",
+                Email = "test@test.com",
+                City = "Albany",
+                PhoneNumber = "(999)123-4567",
+                DOB = new DateTime(2000, 12, 5),
+                State = "New York",
+                Country = "United States",
+                ZipCode = "12345"
+            };
+            var validationResultList = new List<ValidationResult>();
+            Validator.TryValidateObject(cusotmer, new ValidationContext(cusotmer), validationResultList);
+            var message = validationResultList[0].ErrorMessage;
+            Assert.AreEqual("Street Address 1 is required", message);
+        }
+        [TestMethod]
+        public void Test_City_Invalid()
+        {
+            User cusotmer = new User
+            {
+                FirstName = "John",
+                LastName = "David",
+                Email = "test@test.com",
+                PhoneNumber = "(999)123-4567",
+                DOB = new DateTime(2000, 12, 5),
+                State = "New York",
+                Country = "United States",
+                StreetAddress1 = "Test",
+                ZipCode = "12345"
+            };
+            var validationResultList = new List<ValidationResult>();
+            Validator.TryValidateObject(cusotmer, new ValidationContext(cusotmer), validationResultList);
+            var message = validationResultList[0].ErrorMessage;
+            Assert.AreEqual("City is required", message);
+        }
+
+        [TestMethod]
+        public void Test_State_Invalid()
+        {
+            User cusotmer = new User
+            {
+                FirstName = "John",
+                LastName = "David",
+                Email = "test@test.com",
+                City = "Albany",
+                PhoneNumber = "(999)123-4567",
+                DOB = new DateTime(2000, 12, 5),
+                State = "abc",
+                Country = "United States",
+                StreetAddress1 = "Test",
+                ZipCode = "12345"
+            };
+            var validationResultList = new List<ValidationResult>();
+            Validator.TryValidateObject(cusotmer, new ValidationContext(cusotmer), validationResultList);
+            var message = validationResultList[0].ErrorMessage;
+            Assert.AreEqual("Invalid State", message);
+        }
+        [TestMethod]
+        public void Test_Country_Invalid()
+        {
+            User cusotmer = new User
+            {
+                FirstName = "John",
+                LastName = "David",
+                Email = "test@test.com",
+                City = "Albany",
+                PhoneNumber = "(999)123-4567",
+                DOB = new DateTime(2000, 12, 5),
+                State = "New York",
+                Country = "Abc",
+                StreetAddress1 = "Test",
+                ZipCode = "12345"
+            };
+            var validationResultList = new List<ValidationResult>();
+            Validator.TryValidateObject(cusotmer, new ValidationContext(cusotmer), validationResultList);
+            var message = validationResultList[0].ErrorMessage;
+            Assert.AreEqual("Invalid Country", message);
+        }
+        [TestMethod]
+        public void Test_DOB_Future_Date()
+        {
+            User cusotmer = new User
+            {
+                FirstName = "John",
+                LastName = "David",
+                Email = "test@test.com",
+                City = "Albany",
+                PhoneNumber = "(999)123-4567",
+                DOB = DateTime.Now.AddDays(100),
+                State = "New York",
+                Country = "United States",
+                StreetAddress1 = "Test",
+                ZipCode = "12345"
+            };
+            var validationResultList = new List<ValidationResult>();
+            Validator.TryValidateObject(cusotmer, new ValidationContext(cusotmer), validationResultList);
+            var message = validationResultList[0].ErrorMessage;
+            Assert.AreEqual("Invalid Date Of Birth", message);
+        }
+        [TestMethod]
+        public void Test_Validate_All()
+        {
+            User cusotmer = new User
+            {
+                FirstName = "John",
+                LastName = "David",
+                Email = "test@test.com",
+                City = "Albany",
+                PhoneNumber = "(999)123-4567",
+                DOB = new DateTime(2000, 12, 5),
+                State = "New York",
+                Country = "United States",
+                StreetAddress1 = "Test",
+                ZipCode = "12345"
+            };
+            var validationResultList = new List<ValidationResult>();
+            Validator.TryValidateObject(cusotmer, new ValidationContext(cusotmer), validationResultList);
+            Assert.AreEqual(validationResultList.Any(), false);
+        }
+
+       
     }
 }
+
--- WebApp/Models/User.cs
+++ WebApp/Models/User.cs
@@ -5,56 +5,155 @@ using System.Web.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
+using System.Text.RegularExpressions;
 
 namespace WebApp.Models
 {
-    public class User
+    public class User :IValidatableObject
     {
         [Key, Column(Order = 1)]
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "First Name is required")]
         [StringLength(50, MinimumLength = 3)]
+
         public string FirstName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Last Name is required")]
         [StringLength(50, MinimumLength = 3)]
         public string LastName { get; set; }
+        
         [Required]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage ="Invalid Email")]
         public string Email { get; set; }
 
-        [Required]
+       
+
         [MinLength(6, ErrorMessage = "Password must minimum 6 characters")]
 
         public string Password { get; set; }
 
         [NotMapped]
-        [Required]
         [System.ComponentModel.DataAnnotations.Compare("Password")]
         public string ConfirmPassword { get; set; }
         public string FullName()
         {
             return this.FirstName + " " + this.LastName;
         }
-        [Required]
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+
+            //Email
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(Email);
+            if (!match.Success)
+            {
+                yield return new ValidationResult("Invalid Email", new[] { "Email" });
+            }
+
+            var phoneRegex = new Regex(@"^((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}$");
+            match = phoneRegex.Match(PhoneNumber);
+            if (!match.Success)
+            {
+                yield return new ValidationResult("Invalid PhoneNumber", new[] { "PhoneNumber" });
+            }
+
+            if (Country !=null && !Country.Equals("United States"))
+            {
+                yield return new ValidationResult("Invalid Country", new[] { "Country" });
+            }
+
+            if (Country != null && !states.Contains (State))
+            {
+                yield return new ValidationResult("Invalid State", new[] { "State" });
+            }
+
+
+          
+            int comparedate = DateTime.Compare(DateTime.Now, DOB);
+
+            if (comparedate < 0)
+            {
+                yield return new ValidationResult("Invalid Date Of Birth", new[] { "DOB" });
+
+            }
+
+        }
+
+        [Required(ErrorMessage = "Street Address 1 is required")]
         public string StreetAddress1 { get; set; }
 
-        [Required]
         public string StreetAddress2 { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "City is required")]
         public string City { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "State is required")]
         public string State { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Country is required")]
         public string Country { get; set; }
         [Required]
         public string ZipCode { get; set; }
         [Required]
-        [Phone]
+        [Phone(ErrorMessage ="Invalid PhoneNumber")]
         public string PhoneNumber { get; set; }
         [Required]
         public DateTime DOB { get; set; }
+
+        private String[] states = {"Alaska",
+                  "Alabama",
+                  "Arkansas",
+                  "American Samoa",
+                  "Arizona",
+                  "California",
+                  "Colorado",
+                  "Connecticut",
+                  "District of Columbia",
+                  "Delaware",
+                  "Florida",
+                  "Georgia",
+                  "Guam",
+                  "Hawaii",
+                  "Iowa",
+                  "Idaho",
+                  "Illinois",
+                  "Indiana",
+                  "Kansas",
+                  "Kentucky",
+                  "Louisiana",
+                  "Massachusetts",
+                  "Maryland",
+                  "Maine",
+                  "Michigan",
+                  "Minnesota",
+                  "Missouri",
+                  "Mississippi",
+                  "Montana",
+                  "North Carolina",
+                  " North Dakota",
+                  "Nebraska",
+                  "New Hampshire",
+                  "New Jersey",
+                  "New Mexico",
+                  "Nevada",
+                  "New York",
+                  "Ohio",
+                  "Oklahoma",
+                  "Oregon",
+                  "Pennsylvania",
+                  "Puerto Rico",
+                  "Rhode Island",
+                  "South Carolina",
+                  "South Dakota",
+                  "Tennessee",
+                  "Texas",
+                  "Utah",
+                  "Virginia",
+                  "Virgin Islands",
+                  "Vermont",
+                  "Washington",
+                  "Wisconsin",
+                  "West Virginia",
+                  "Wyoming" };
     }
 }
